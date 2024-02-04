5a4e06aa9d249e4d693ff83cb57686854557bc92
Carl Blome <techsupport332@comcast.net>
2024-02-04 13:37:09 -0500

    started adding todos I should start doing my other hw though

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 
 /**
+ * TODO: Main program logic goes in this file each individual function of our drivers should be handled in their individual Driver Files
  * main.c
  */
 void main(void)
@@ -9,3 +10,4 @@ void main(void)
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	//howdy
 }
+
---
+++ main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: Feb 4, 2024
+ *      Author: blomecj
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//TODO: All the ports and different port things should happen in here
+
+
+#endif /* MAIN_H_ */
