8f85044988b4f34d97cd67adc8518d304e6e4644
Carl Blome <techsupport332@comcast.net>
2024-02-06 21:27:37 -0500

    added some todos to LCDDriver

--- LCDDriver.c
+++ LCDDriver.c
@@ -6,3 +6,17 @@
  */
 
 
+//TODO LCDconfig(#LCDPort)
+
+//TODO LCDinit(#LCDPort) this should run the initialization commands I didn't get 4 bit to work so we are just doing an 8 bit driver unfortunately
+
+//TODO LCDWrite (#LCDPort, char (just a one byte number), INSTR or DATA modes)
+
+//TODO PrintChar(#LCDPort, char); calls LCDWrite with this and DATA mode
+
+//TODO PrintString(#LCDPort, char *); iterate through chars calling Print Char allow a certain delay between chars
+//  TODO also enable LCD scrolling if song name or artist name will run off the display (check if the string being sent to LCD will run out of space (maybe make main in charge of this with an interupt handler and make a function that shifts the LCD)
+
+//TODO Add fucntions that manipulate address to change line and position on screen when we want or just display things where we want them
+
+
