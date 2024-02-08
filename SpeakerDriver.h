528d8b555464941097e091391ff1f238103b1742
Carl Blome <techsupport332@comcast.net>
2024-02-07 23:15:04 -0500

    Added speaker FreqClock defines

--- SpeakerDriver.h
+++ SpeakerDriver.h
@@ -8,7 +8,9 @@
 #ifndef SPEAKERDRIVER_H_
 #define SPEAKERDRIVER_H_
 
-//TODO will need clock speed I'm setting the FreqTimer to
+//will need clock speed I'm setting the FreqTimer to
 //after prescalar SpeakerFreqClock should be 1MHz
+#define SpeakerFreqClockPrescalar 48
+#define SpeakerFreqClockFreq 48000000/SpeakerFreqClockPrescalar //since using our normal HFXT configuration the clock will be 48000000
 
 #endif /* SPEAKERDRIVER_H_ */
