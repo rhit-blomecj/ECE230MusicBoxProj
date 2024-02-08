ad18799c131bd8a160c3ac223e719d69fd638a81
Carl Blome <techsupport332@comcast.net>
2024-02-07 22:22:28 -0500

    Made decision on range of notes and NoteDurationFreq

--- NoteDefs.h
+++ NoteDefs.h
@@ -8,8 +8,38 @@
 #ifndef NOTEDEFS_H_
 #define NOTEDEFS_H_
 //TODO add Note Frequencies so our Music can use these defines to have proper values
+//https://mixbutton.com/mixing-articles/music-note-to-frequency-chart/
 #define R 1 //rest # of ticks
 
+#define FrequencyA0 27.50 //Hz
+#define FrequencyAS0  //Hz
+#define FrequencyB0  //Hz
+#define FrequencyC1  //Hz
+#define FrequencyCS1  //Hz
+#define FrequencyD1  //Hz
+#define FrequencyDS1  //Hz
+#define FrequencyE1  //Hz
+#define FrequencyF1  //Hz
+#define FrequencyFS1  //Hz
+#define FrequencyG1  //Hz
+#define FrequencyGS1  //Hz
+#define FrequencyA1  //Hz
+#define FrequencyAS1  //Hz
+#define FrequencyB1  //Hz
+#define FrequencyC2  //Hz
+#define FrequencyCS2  //Hz
+#define FrequencyD2  //Hz
+#define FrequencyDS2  //Hz
+#define FrequencyE2  //Hz
+#define FrequencyF2  //Hz
+#define FrequencyFS2  //Hz
+#define FrequencyG2  //Hz
+#define FrequencyGS2  //Hz
+#define FrequencyA2  //Hz
+#define FrequencyAS2  //Hz
+#define FrequencyB2  //Hz
+#define FrequencyC3  //Hz
+#define FrequencyCS3  //Hz
 #define FrequencyD3 146.83 //Hz
 #define FrequencyDS3 155.56 //Hz
 #define FrequencyE3 164.81 //Hz
@@ -38,6 +68,37 @@
 #define FrequencyDS5 622.25 //Hz
 #define FrequencyE5 659.25 //Hz
 #define FrequencyF5 698.46 //Hz
+#define FrequencyFS5  //Hz
+#define FrequencyG5  //Hz
+#define FrequencyGS5  //Hz
+#define FrequencyA5  //Hz
+#define FrequencyAS5  //Hz
+#define FrequencyB5  //Hz
+#define FrequencyC6  //Hz
+#define FrequencyCS6  //Hz
+#define FrequencyD6  //Hz
+#define FrequencyDS6  //Hz
+#define FrequencyE6  //Hz
+#define FrequencyF6  //Hz
+#define FrequencyFS6  //Hz
+#define FrequencyG6  //Hz
+#define FrequencyGS6  //Hz
+#define FrequencyA6  //Hz
+#define FrequencyAS6  //Hz
+#define FrequencyB6  //Hz
+#define FrequencyC7  //Hz
+#define FrequencyCS7  //Hz
+#define FrequencyD7  //Hz
+#define FrequencyDS7  //Hz
+#define FrequencyE7  //Hz
+#define FrequencyF7  //Hz
+#define FrequencyFS7  //Hz
+#define FrequencyG7  //Hz
+#define FrequencyGS7  //Hz
+#define FrequencyA7  //Hz
+#define FrequencyAS7  //Hz
+#define FrequencyB7  //Hz
+#define FrequencyC8 4186.01 //Hz
 
 
 
--- main.h
+++ main.h
@@ -19,8 +19,8 @@
 //Speaker Freq Timer should be TimerA0
 //Spearker Freq pins should be P2.4-P2.7 (corresponds to TA0.1-TA0.4)
 #define SpeakerFreqTimer Timer_A0
-#define SpeakerPort P2 //TODO
-#define Speaker1 1<<4 //TODO idk if I want this pin business
+#define SpeakerPort P2
+#define Speaker1 1<<4
 #define Speaker2 1<<5
 #define Speaker3 1<<6
 #define Speaker4 1<<7
@@ -35,7 +35,9 @@
 //Note Duration Timer should be TimerA2
 //Note Duration Timer Pins should be P5.6 P5.7 P6.6 P6.7 corresponds to (TA2.1-TA2.4)
 #define NoteDurationTimer Timer_A2
-#define HighPort P6
+#define NoteDurationTimerFreq 1000000 //Hz we will set it up so this works
+
+#define HighPort P6 //interrupt out ports for Timer_A2
 #define LowPort P5
 
 #define SopranoDur 1<<7
@@ -56,6 +58,7 @@
 #define MagnetSwitchPin 1<<5
 
 //TODO Save Timer A2 clockrate and stuff so we can do bpm and duration calculations
-//quarter note time needs to be figured out during runtime if I want bpms that can change
+//bpm time needs to be figured out during runtime if I want bpms that can change
+//don't calculate quarter note time calculate 1 beat time when calculating
 
 #endif /* MAIN_H_ */
