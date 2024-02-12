ed61aa0e9a02fdbe3c18be5cb8753ffbfe58aef3
bakeref <bakeref@rose-hulman.edu>
2024-02-12 11:09:24 -0500

    El - wrote all note frequencies

--- NoteDefs.h
+++ NoteDefs.h
@@ -7,39 +7,39 @@
 
 #ifndef NOTEDEFS_H_
 #define NOTEDEFS_H_
-//TODO add Note Frequencies so our Music can use these defines to have proper values
+//DONE add Note Frequencies so our Music can use these defines to have proper values
 //https://mixbutton.com/mixing-articles/music-note-to-frequency-chart/
 #define R 1 //rest # of ticks
 
 #define FrequencyA0 27.50 //Hz
-#define FrequencyAS0  //Hz
-#define FrequencyB0  //Hz
-#define FrequencyC1  //Hz
-#define FrequencyCS1  //Hz
-#define FrequencyD1  //Hz
-#define FrequencyDS1  //Hz
-#define FrequencyE1  //Hz
-#define FrequencyF1  //Hz
-#define FrequencyFS1  //Hz
-#define FrequencyG1  //Hz
-#define FrequencyGS1  //Hz
-#define FrequencyA1  //Hz
-#define FrequencyAS1  //Hz
-#define FrequencyB1  //Hz
-#define FrequencyC2  //Hz
-#define FrequencyCS2  //Hz
-#define FrequencyD2  //Hz
-#define FrequencyDS2  //Hz
-#define FrequencyE2  //Hz
-#define FrequencyF2  //Hz
-#define FrequencyFS2  //Hz
-#define FrequencyG2  //Hz
-#define FrequencyGS2  //Hz
-#define FrequencyA2  //Hz
-#define FrequencyAS2  //Hz
-#define FrequencyB2  //Hz
-#define FrequencyC3  //Hz
-#define FrequencyCS3  //Hz
+#define FrequencyAS0 29.14 //Hz
+#define FrequencyB0 30.87 //Hz
+#define FrequencyC1 32.70 //Hz
+#define FrequencyCS1 34.65 //Hz
+#define FrequencyD1 36.71 //Hz
+#define FrequencyDS1 38.89 //Hz
+#define FrequencyE1 41.20 //Hz
+#define FrequencyF1 43.65 //Hz
+#define FrequencyFS1 46.25 //Hz
+#define FrequencyG1 49.00 //Hz
+#define FrequencyGS1 51.91 //Hz
+#define FrequencyA1 55.00 //Hz
+#define FrequencyAS1 58.27 //Hz
+#define FrequencyB1 61.74 //Hz
+#define FrequencyC2 65.41 //Hz
+#define FrequencyCS2 69.30 //Hz
+#define FrequencyD2 73.42 //Hz
+#define FrequencyDS2 77.78 //Hz
+#define FrequencyE2 82.41 //Hz
+#define FrequencyF2 87.31 //Hz
+#define FrequencyFS2 92.50 //Hz
+#define FrequencyG2 98.00 //Hz
+#define FrequencyGS2 103.83 //Hz
+#define FrequencyA2 110.00 //Hz
+#define FrequencyAS2 116.54 //Hz
+#define FrequencyB2 123.47 //Hz
+#define FrequencyC3 130.81 //Hz
+#define FrequencyCS3 138.59 //Hz
 #define FrequencyD3 146.83 //Hz
 #define FrequencyDS3 155.56 //Hz
 #define FrequencyE3 164.81 //Hz
@@ -68,36 +68,36 @@
 #define FrequencyDS5 622.25 //Hz
 #define FrequencyE5 659.25 //Hz
 #define FrequencyF5 698.46 //Hz
-#define FrequencyFS5  //Hz
-#define FrequencyG5  //Hz
-#define FrequencyGS5  //Hz
-#define FrequencyA5  //Hz
-#define FrequencyAS5  //Hz
-#define FrequencyB5  //Hz
-#define FrequencyC6  //Hz
-#define FrequencyCS6  //Hz
-#define FrequencyD6  //Hz
-#define FrequencyDS6  //Hz
-#define FrequencyE6  //Hz
-#define FrequencyF6  //Hz
-#define FrequencyFS6  //Hz
-#define FrequencyG6  //Hz
-#define FrequencyGS6  //Hz
-#define FrequencyA6  //Hz
-#define FrequencyAS6  //Hz
-#define FrequencyB6  //Hz
-#define FrequencyC7  //Hz
-#define FrequencyCS7  //Hz
-#define FrequencyD7  //Hz
-#define FrequencyDS7  //Hz
-#define FrequencyE7  //Hz
-#define FrequencyF7  //Hz
-#define FrequencyFS7  //Hz
-#define FrequencyG7  //Hz
-#define FrequencyGS7  //Hz
-#define FrequencyA7  //Hz
-#define FrequencyAS7  //Hz
-#define FrequencyB7  //Hz
+#define FrequencyFS5 739.99 //Hz
+#define FrequencyG5 783.99 //Hz
+#define FrequencyGS5 830.61 //Hz
+#define FrequencyA5 880.00 //Hz
+#define FrequencyAS5 932.33 //Hz
+#define FrequencyB5 987.77 //Hz
+#define FrequencyC6 1046.50 //Hz
+#define FrequencyCS6 1108.73 //Hz
+#define FrequencyD6 1174.66 //Hz
+#define FrequencyDS6 1244.51 //Hz
+#define FrequencyE6 1318.51 //Hz
+#define FrequencyF6 1396.91 //Hz
+#define FrequencyFS6 1479.98 //Hz
+#define FrequencyG6 1567.98 //Hz
+#define FrequencyGS6 1661.22 //Hz
+#define FrequencyA6 1760.00 //Hz
+#define FrequencyAS6 1864.66 //Hz
+#define FrequencyB6 1975.53 //Hz
+#define FrequencyC7 2093.00 //Hz
+#define FrequencyCS7 2217.46 //Hz
+#define FrequencyD7 2349.32 //Hz
+#define FrequencyDS7 2489.02 //Hz
+#define FrequencyE7 2637.02 //Hz
+#define FrequencyF7 2793.83 //Hz
+#define FrequencyFS7 2959.96 //Hz
+#define FrequencyG7 3135.96 //Hz
+#define FrequencyGS7 3322.44 //Hz
+#define FrequencyA7 3520.00 //Hz
+#define FrequencyAS7 3729.31 //Hz
+#define FrequencyB7 3951.07 //Hz
 #define FrequencyC8 4186.01 //Hz
 
 
